35d8ca8163c0c018c976d9faae44c343239741bd
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-08-03 09:59:00 +0200

    Created the visual class for the graphic part of the program

---
+++ Visual.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess
+{
+    /// <summary>
+    /// Class that contains the methods for the visual part of the program.
+    /// </summary>
+    class Visual
+    {
+    }
+}
