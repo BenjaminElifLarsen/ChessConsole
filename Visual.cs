7635a9709ea2103632e0479dd23d415fd71a5697
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-08-04 11:25:05 +0200

    Added string[] and PointF[] functions and get properties for design. Also implemted a little code for the PaintCalculation function

--- Visual.cs
+++ Visual.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Dynamic;
 using System.Net;
 using System.Text;
 
@@ -12,30 +14,134 @@ namespace Chess
     {
         //how to do this...
         //should this class hold the designs?
+        //should "paint" to the console and to a graphical user interfaces
+        //should the GUI use signs for the visual representation or should it use polygons(PointF[])/bitmaps?
 
+        /// <summary>
+        /// 
+        /// </summary>
         static public void ChessBoardPaint()
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         static public void SquareBackground()
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         static public void SquareForeground()
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         static public void SquareFull()
         {
 
         }
 
-        private static class Designs
+        /// <summary>
+        /// 
+        /// </summary>
+        static public void SquareHighLight()
         {
 
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static public void RemoveDraw()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="drawLocationX"></param>
+        /// <param name="drawLocationY"></param>
+        /// <param name="mapLoc"></param>
+        /// <param name="design"></param>
+        /// <param name="mapLocation"></param>
+        /// <returns></returns>
+        static private byte[] PaintCalculations(out int drawLocationX, out int drawLocationY, int[] mapLoc, string[] design, byte[] mapLocation)
+        {
+            int[] location = new int[2] { mapLocation[0] * Settings.SquareSize + (mapLocation[0] + Settings.EdgeSpacing + Settings.Spacing) * 1 + Settings.Offset[0], mapLocation[1] * Settings.SquareSize + (mapLocation[1] + Settings.EdgeSpacing + Settings.Spacing) * 1 + Settings.Offset[1] };
+            int designSize = design.Length;
+            drawLocationX = location[0] + (int)(Settings.SquareSize - designSize) / 2;
+            drawLocationY = location[1] + (int)(Settings.SquareSize - designSize) / 2;
+            int locationForColour = (mapLoc[0] + mapLoc[1]) % 2; //if zero, background colour is "white", else background colour is "black".
+            byte[] colours = locationForColour == 0 ? Settings.SquareColour1 : Settings.SquareColour2;
+            return colours;
+        }
+
+        static private byte[] PaintCalculations(out int drawLocationX, out int drawLocationY, int[] mapLoc,  byte[] mapLocation)
+        {
+            drawLocationX = 0;
+            drawLocationY = 0;
+            return null;
+        }
+
+        private static class Designs
+        {
+            //bishop
+            /// <summary>
+            /// 
+            /// </summary>
+            private static string[] bishopCmdDesign =
+            {
+
+            };
+            /// <summary>
+            /// 
+            /// </summary>
+            private static PointF[] bishopGUIDesign =
+            { //might be best to contain all designs in this class rather than the different chess piece classes to prevent having to change them when changing their design.
+
+            };
+            /// <summary>
+            /// 
+            /// </summary>
+            public static string[] CMD_Bishop { get => bishopCmdDesign; }
+            /// <summary>
+            /// 
+            /// </summary>
+            public static PointF[] GUI_Bishop { get => bishopGUIDesign; }
+
+            //--------------------------------
+
+            /// <summary>
+            /// 
+            /// </summary>
+            private static string[] pawnCMDDesign =
+            {
+
+            };
+            /// <summary>
+            /// 
+            /// </summary>
+            private static PointF[] pawnGUIDesign =
+            {
+
+            };
+            /// <summary>
+            /// 
+            /// </summary>
+            public static string[] CMD_Pawn { get => pawnCMDDesign; }
+            /// <summary>
+            /// 
+            /// </summary>
+            public static PointF[] GUI_Pawn { get => pawnGUIDesign; }
 
         }
     }
