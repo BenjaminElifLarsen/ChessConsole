07c5ba639a7f26a459cd411bfa18b3e687ab8fa0
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-08-13 13:48:15 +0200

    reverted to fix an error and reimplemented the array support

--- Reporter.cs
+++ Reporter.cs
@@ -15,18 +15,23 @@ namespace Chess
         private static readonly string filename = "Error";
         private static readonly string filetype = ".txt";
 
+        /// <summary>
+        /// Writes out <paramref name="e"/> to a text file. 
+        /// </summary>
+        /// <param name="e"></param>
         public static void Report(Exception e)
         {
 
             DateTime time = DateTime.Now;
-            string newFileName = $"{filename} {time.Year}-{time.Month}-{time.Day}-{time.Hour}-{time.Minute}-{time.Second}-{time.Millisecond}{filetype}";
-            //string newFileName = filename + filetype;
+            //string newFileName = $"{filename} {time.Year}-{time.Month}-{time.Day}-{time.Hour}-{time.Minute}-{time.Second}-{time.Millisecond}{filetype}";
+            string newFileName = filename + filetype;
+            string timePoint = $"{filename} {time.Year}-{time.Month}-{time.Day}-{time.Hour}-{time.Minute}-{time.Second}-{time.Millisecond}";
             CreateFolder(pathway);
             CreateFile(pathway, newFileName);
             string pathFile = Path.Combine(pathway, newFileName);
             using (StreamWriter file = new StreamWriter(pathFile))
             {
-                file.WriteLine(time);
+                file.WriteLine(timePoint);
                 file.Write(Environment.NewLine);
                 file.WriteLine(e.Source);
                 file.Write(Environment.NewLine);
@@ -44,6 +49,10 @@ namespace Chess
             }
         }
 
+        /// <summary>
+        /// If the error folder <paramref name="path"/> does not exist create it.
+        /// </summary>
+        /// <param name="path">Path and name of the error folder.</param>
         private static void CreateFolder(string path)
         {
             Debug.WriteLine(path);
@@ -51,6 +60,11 @@ namespace Chess
                 Directory.CreateDirectory(path);
         }
         
+        /// <summary>
+        /// If the error filer <paramref name="file"/> does not exist create it at <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">The path to the error file.</param>
+        /// <param name="file">Name and filetype of the error file.</param>
         private static void CreateFile(string path, string file)
         {
             string pathFile = Path.Combine(path, file);
